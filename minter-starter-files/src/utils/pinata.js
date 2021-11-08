ce4b340ccba3b80bba74ab2f8929deee592c3723
Aleksandra Kleczek <aleksandra.kleczek@openx.com>
2021-11-08 21:30:13 +0100

    first commit

--- minter-starter-files/src/utils/pinata.js
+++ minter-starter-files/src/utils/pinata.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const key = process.env.REACT_APP_PINATA_KEY;
 const secret = process.env.REACT_APP_PINATA_SECRET;
-const axios = require('axios');
 
+const axios = require('axios');
 export const pinJSONToIPFS = async(JSONBody) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
     return axios
         .post(url, JSONBody, {
             headers: {
@@ -13,10 +14,10 @@ export const pinJSONToIPFS = async(JSONBody) => {
             }
         })
         .then(function (response) {
-           return {
-               success: true,
-               pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
-           };
+            return {
+                success: true,
+                pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+            };
         })
         .catch(function (error) {
             console.log(error)
@@ -24,6 +25,5 @@ export const pinJSONToIPFS = async(JSONBody) => {
                 success: false,
                 message: error.message,
             }
-           
         });
 };
